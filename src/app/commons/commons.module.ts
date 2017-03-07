69ed79854e5f9459b0b9fb30fa1f5e827eb09046
tolstyak <epamprojects@gmail.com>
2017-03-08 01:37:29 +0200

    Aligning vertical template  for basic components .

--- src/app/commons/commons.module.ts
+++ src/app/commons/commons.module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {CommonVerticalSetWindowHeightDirective} from './directives';
 
 @NgModule({
   imports: [
     CommonModule
   ],
+  declarations: [CommonVerticalSetWindowHeightDirective],
+  exports: [CommonVerticalSetWindowHeightDirective]
 })
 export class CommonsModule { }
+
---
+++ src/app/commons/directives/index.ts
@@ -0,0 +1,1 @@
+export {CommonVerticalSetWindowHeightDirective} from './common-vertical-align.directive'
